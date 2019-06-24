e4b5f374d9382211c2807302252a15d66fe2adf9
Bruno Mendonça Goldoni <bgoldoni@orion.tech>
2019-06-24 13:45:02 -0300

    View Child

--- AngularApp/src/app/parent-child/parent-child.component.ts
+++ AngularApp/src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  @ViewChild("stopwatchtwo")
+  private timer: TimerComponent;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  start() {
+    this.timer.start();
+  }
+
+  stop() {
+    this.timer.stop();
+  }
+
+  clear() {
+    this.timer.clear();
+  }
 }
